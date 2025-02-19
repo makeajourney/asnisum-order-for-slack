4993058d58eaf4c39b31461db7614d892288e282
김소연/회원개발팀 <soyoun.kim@musinsa.com>
2025-02-19 22:11:00 +0900

    doc: 도움말에 메뉴 업데이트 방법 추가

--- blocks/tutorial.js
+++ blocks/tutorial.js
@@ -1,62 +1,65 @@
-// blocks/tutorial.js
 const getTutorialBlocks = () => [
   {
-    type: 'section',
+    type: 'header',
     text: {
-      type: 'mrkdwn',
-      text: '*아즈니섬 주문봇 사용 가이드* 📝',
-    },
-  },
-  {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: '*시작하기 전에*\n주문을 사용하려는 채널에 🤖 *아즈니섬주문* 봇을 초대해주세요.\n한 채널당 하나의 주문만 유효합니다. 기존 주문을 완료하고 `주문시작`명령어를 사용해 새로운 주문을 시작하세요.',
-    },
+      type: 'plain_text',
+      text: '🍵 아즈니섬 주문봇 사용 가이드',
+      emoji: true
+    }
   },
   {
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: '다음 명령어들을 사용할 수 있습니다:',
-    },
+      text: '*시작하기 전에*\n주문을 사용하려는 채널에 🤖 *아즈니섬주문* 봇을 초대해주세요.\n한 채널당 하나의 주문만 유효합니다. 기존 주문을 완료하고 `/아즈니섬 주문시작` 명령어를 사용해 새로운 주문을 시작하세요.'
+    }
+  },
+  {
+    type: 'divider'
   },
   {
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: '• `/아즈니섬 주문시작` - 새로운 주문 세션을 시작합니다\n• `/아즈니섬 주문마감` - 현재 진행 중인 주문을 마감하고 주문 내역을 보여줍니다\n• `/아즈니섬 도움말` - 이 도움말을 보여줍니다',
-    },
-  },
-  {
-    type: 'divider',
+      text: '*사용 가능한 명령어*'
+    }
   },
   {
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: '*주문 과정 예시:*',
-    },
+      text: '• `/아즈니섬 주문시작`\n   새로운 주문 세션을 시작합니다\n\n• `/아즈니섬 주문마감`\n   현재 진행 중인 주문을 마감하고 주문 내역을 보여줍니다\n\n• `/아즈니섬 도움말`\n   이 도움말을 보여줍니다'
+    }
+  },
+  {
+    type: 'divider'
   },
   {
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: "0️⃣ `아즈니섬주문 봇을 채널에 초대합니다\n1️⃣ `/아즈니섬 주문시작` 명령어로 주문 세션을 시작합니다\n2️⃣ '주문하기' 버튼을 클릭하여 메뉴를 입력합니다\n3️⃣ 모든 주문이 완료되면 `/아즈니섬 주문마감` 명령어로 주문을 마감합니다",
-    },
+      text: '*주문 과정 예시*'
+    }
   },
   {
-    type: 'divider',
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: '0️⃣ 아즈니섬주문 봇을 채널에 초대합니다\n1️⃣ `/아즈니섬 주문시작` 명령어로 주문 세션을 시작합니다\n2️⃣ \'주문하기\' 버튼을 클릭하여 메뉴를 입력합니다\n3️⃣ 모든 주문이 완료되면 `/아즈니섬 주문마감` 명령어로 주문을 마감합니다'
+    }
+  },
+  {
+    type: 'divider'
   },
   {
     type: 'context',
     elements: [
       {
         type: 'mrkdwn',
-        text: '사용문의 <@U032432TM0Q>\n이슈 등록: <https://github.com/makeajourney/asnisum-order-for-slack/issues|GitHub>',
-      },
-    ],
-  },
+        text: '💁 사용문의: <@U032432TM0Q>\n📌 이슈 등록: <https://github.com/makeajourney/asnisum-order-for-slack/issues|GitHub>\n📖 메뉴 설정 가이드: <https://github.com/makeajourney/asnisum-order-for-slack?tab=readme-ov-file#메뉴-설정-가이드|가이드 보기>'
+      }
+    ]
+  }
 ];
 
 const errorMessages = {
