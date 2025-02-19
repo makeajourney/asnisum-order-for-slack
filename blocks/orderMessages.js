2179e1c5881c8bc7b8b6bbf64a2031e266060f0f
김소연/회원개발팀 <soyoun.kim@musinsa.com>
2025-02-19 22:45:13 +0900

    feat: 주문 시작 시 사용자 그룹 태그 기능 추가

---
+++ blocks/orderMessages.js
@@ -0,0 +1,58 @@
+const orderMessages = {
+    start: {
+      text: '아즈니섬 음료 주문 받습니다! ☕️🥤',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '☕️ *아즈니섬 음료 주문 받습니다!* 🥤 아즈니섬 음료를 주문하실 분들은 아래 주문하기 버튼을 눌러주세요. :pepe-coffee:',
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: '주문하기',
+                emoji: true,
+              },
+              action_id: 'order_button',
+            },
+          ],
+        },
+      ]
+    },
+    withUserGroup: {
+      text: (userGroupId) => `<!subteam^${userGroupId}> 아즈니섬 음료 주문 받습니다! ☕️🥤`,
+      blocks: (userGroupId) => [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `️🥤 <!subteam^${userGroupId}> *아즈니섬 음료 주문 받습니다!* ☕️ 아즈니섬 음료를 주문하실 분들은 아래 주문하기 버튼을 눌러주세요. :pepe-coffee:`,
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: '주문하기',
+                emoji: true,
+              },
+              action_id: 'order_button',
+            },
+          ],
+        },
+      ]
+    }
+  };
+  
+  module.exports = {
+    orderMessages
+  };
